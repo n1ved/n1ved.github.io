da77f04538ebc643e9c066e7352e0c256a343044
n1ved <nivedrsalini@gmail.com>
2023-04-22 17:18:54 +0530

    navbar function , visual adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,22 @@ window.onload = function () {
     // }, 250);
 
 }
+
+function navbarselected(screen) {
+
+    var colors = [
+        "#1e1e1e",
+        "#0000003a"
+    ]
+    var navlinks = document.querySelectorAll('.navlink');
+    console.log(navlinks);
+    navlinks.forEach(navlink => {
+        var x = document.getElementById(navlink.id);
+        if (navlink.id == screen) {
+            x.style.color = colors[0];
+        }
+        else {
+            x.style.color = colors[1];
+        }
+    });
+}
