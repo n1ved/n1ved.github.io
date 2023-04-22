c592151c76bacf782d83169d8dbfc3ca7ec2ab6e
n1ved <nivedrsalini@gmail.com>
2023-04-22 10:58:27 +0530

    Update aboutpage

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+
+window.onload = function () {
+
+    var fontArray = [
+        'JetBrains Mono',
+        'Roboto',
+        'Dancing Script',
+        'Bruno Ace',
+        'Castoro Titling',
+        'Monoton'
+    ]
+
+    // var interval = setInterval(function () {
+    //     var randomnum = Math.floor(Math.random() * fontArray.length + 1);
+    //     document.getElementById("hero").style.fontFamily = fontArray[randomnum];
+    // }, 250);
+
+}
