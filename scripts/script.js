018fafac4d458301de380c1662c8a650999bcebc
n1ved <nivedrsalini@gmail.com>
2023-04-26 20:43:44 +0530

    Cleaned files

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,4 @@
 
-window.onload = function () {
-
-    var fontArray = [
-        'JetBrains Mono',
-        'Roboto',
-        'Dancing Script',
-        'Bruno Ace',
-        'Castoro Titling',
-        'Monoton'
-    ]
-
-    // var interval = setInterval(function () {
-    //     var randomnum = Math.floor(Math.random() * fontArray.length + 1);
-    //     document.getElementById("hero").style.fontFamily = fontArray[randomnum];
-    // }, 250);
-
-}
-
 function navbarselected(screen) {
 
     var colors = [
