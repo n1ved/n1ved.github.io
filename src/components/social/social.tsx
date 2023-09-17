973e6c4eef9ac8763fab8dba402a5e7780a8623c
n1ved <nivedrsalini@gmail.com>
2023-09-17 21:18:09 +0530

    Fix mastodon link

--- src/components/social/social.tsx
+++ src/components/social/social.tsx
@@ -10,7 +10,7 @@ export default function Social() {
     return <div className={style.social}>
         <a href='https://github.com/n1ved/' className={style.link}><FontAwesomeIcon icon={faGithub} className={style.icon}/></a>
         <a href='https://linkedin.com/in/n1ved' className={style.link}><FontAwesomeIcon icon={faLinkedin} className={style.icon}/></a>
-        <a href='https://fosstodon.com/@n1ved' className={style.link}><FontAwesomeIcon icon={faMastodon} className={style.icon}/></a>
+        <a href='https://fosstodon.org/@n1ved' className={style.link}><FontAwesomeIcon icon={faMastodon} className={style.icon}/></a>
         <a href='https://instagram.com/__n1ved' className={style.link}><FontAwesomeIcon icon={faInstagram} className={style.icon}/></a>
         <a href='mailto:nivedrsalini@gmail.com' className={style.link}><FontAwesomeIcon icon={faEnvelope} className={style.icon}/></a>
         <a href='https://t.me/n1ved_0' className={style.link}><FontAwesomeIcon icon={faTelegram} className={style.icon}/></a>
