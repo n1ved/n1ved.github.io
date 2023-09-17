36038ea819e9d47696f126c114e6d8252d40d91c
n1ved <nivedrsalini@gmail.com>
2023-09-17 22:38:48 +0530

    Added more projects gh link

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,24 +3,27 @@ import styles from './page.module.css'
 import ProjectCard from "@/components/project_card/project_card";
 export default function Projects() {
     return <main className={styles.main}>
-        <Navbar/>
+        <Navbar haveTitle={true} title={"Projects"}/>
         <div className={styles.bodyContainer}>
             <ProjectCard
                 className={styles.projectCard}
                 title="Skyze"
                 description="Skyze is a weather app made on flutter with openweathermap API"
-                technologies="Flutter, Dart, REST API"
+                technologies="Flutter, Dart"
                 github = "https://github.com/n1ved/skyze"
-                live = ""
+                haveLive = {false}
+                haveDownload = {true}
                 download = "https://github.com/n1ved/skyze/releases/latest"
-                colors="#19B9FB,cyan,grey"
+                colors="#19B9FB,cyan"
             />
             <ProjectCard
                 className={styles.projectCard}
                 title="FAdj"
                 description="Simple front-end for ryzenAdj made with Flutter"
                 technologies="Flutter, Dart, Shell"
-                github = ""
+                github = "https://github.com/n1ved/FAdj"
+                haveLive = {false}
+                haveDownload = {false}
                 colors="#19B9FB,cyan,grey"
             />
             <ProjectCard
@@ -28,11 +31,16 @@ export default function Projects() {
                 title="nCrypter"
                 description="A Simple text cyphering tool written in C++"
                 technologies="C++"
-                links="github.com"
+                links="https://github.com/n1ved/ncrypter"
+                haveLive = {true}
+                live = "https://n1ved.me/ncrypter/"
+                haveDownload = {true}
+                download = "https://github.com/n1ved/ncrypter/releases/latest"
                 colors="pink"
             />
 
         </div>
+        <center> <div className={styles.repo}>Find More on my <a href="https://github.com/n1ved?tab=repositories"> Github</a></div></center>
 
     </main>;
 }
