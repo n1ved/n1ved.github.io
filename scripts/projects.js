30043282c770f07e72c8bd081a5f30ceac221158
n1ved <nivedrsalini@gmail.com>
2024-04-22 20:51:37 +0530

    Okey this might need next

---
+++ scripts/projects.js
@@ -0,0 +1,26 @@
+let projects = [
+    {
+        name: "Skyze",
+        description: "A simple weather app that uses the OpenWeatherMap API and built on Flutter.",
+        image: "skyze.jpg",
+        github: "https://github.com/n1ved/skyze",
+        download: "https://github.com/n1ved/skyze/releases/latest/",
+        technologies: ["Flutter", "Dart"]
+    },
+    {
+        name: "nearPG",
+        description: "A simple website that helps you find PGs near you",
+        image: "nearpg.jpg",
+        github: "https://github.com/n1ved/nearpg",
+        website: "https://nearpg.vercel.app/",
+        technologies: ["React", "Firebase"]
+    },
+    {
+        name: "ncrypter",
+        description: "A simple text ciphering tool written in C++",
+        image: "ncrypter.jpg",
+        github: "https://github.com/n1ved/ncrypter",
+        download: "https://github.com/n1ved/ncrypter/releases/latest/",
+        technologies: ["C++"]
+    }
+];
